a743b97193e89db231668ff6a448d84559d18c19
Hossein Molavi <75816912+hmolavi@users.noreply.github.com>
2023-11-15 01:29:46 -0500

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -7,7 +7,7 @@
  * 					    The RTOS will be utilized to manage concurrent tasks, ensuring timely and
  * 						accurate response to fire-related events.
  *
- * written by Hossein Molavi
+ * Written by Hossein Molavi
  ******************************************************************************
  * @attention
  *
@@ -26,6 +26,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "stm32f4xx_hal.h"
 #include <string.h>
 #include <stdio.h>
 /* USER CODE END Includes */
@@ -48,9 +49,17 @@
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart2;
 UART_HandleTypeDef huart6;
-
+osThreadId SensorMonitoringTaskHandle;
+osThreadId AlarmManagementTaskHandle;
+osThreadId CommunicationTaskHandle;
+osThreadId UserInterfaceTaskHandle;
+ADC_HandleTypeDef hadc1;
 /* USER CODE BEGIN PV */
-UART_HandleTypeDef huart6;
+volatile int FIRE_DETECTED = 0;
+uint8_t uart_txd_buffer[100] = {0};
+uint32_t adcValue = 0;
+uint32_t mappedValue = 0;
+const uint32_t FIRE_THRESHOLD = 800;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -59,19 +68,21 @@ static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_USART6_UART_Init(void);
 /* USER CODE BEGIN PFP */
-static void MX_USART6_UART_Init(void);
+void sensor_monitoring_task_init(void const *argument);
+void alarm_management_task_init(void const *argument);
+void communication_task_init(void const *argument);
+void ui_task_init(void const *argument);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-uint8_t uart_txd_buffer[100] = { 0 };
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -99,38 +110,73 @@ int main(void)
   MX_USART2_UART_Init();
   MX_USART6_UART_Init();
   /* USER CODE BEGIN 2 */
+
+  InitializePin(GPIOA, GPIO_PIN_5, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0); // on-board LED
+  InitializePin(GPIOC, GPIO_PIN_0, GPIO_MODE_INPUT, GPIO_PULLUP, 0);
+  InitializePin(GPIOC, GPIO_PIN_1, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);
+  InitializePin(GPIOA, GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, 0);
+
+  //          gpioport        rs          rw      enable           d4         d5          d6           d7
+  LiquidCrystal(GPIOB, GPIO_PIN_9, GPIO_PIN_8, GPIO_PIN_6, GPIO_PIN_10, GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_3);
+
+  // LCD's number of columns, rows. Set cursor to (0,0) cordinate
+  begin(16, 2);
+  setCursor(0, 0);
+
+  // serial setup
+  SerialSetup(9600);
+  SerialPuts("System Starting\r\n\n");
+  sprintf((char *)txd_msg_buffer, "HOURS\tMINS\tSECS\r\n %d\t %d\t %d\r\n", hours, minutes, seconds);
+  HAL_UART_Transmit(&huart6, txd_msg_buffer, strlen((char *)txd_msg_buffer), 1000);
+
   MX_USART6_UART_Init();
+  osThreadDef(sensor_monitoring_task, sensor_monitoring_task_init, osPriorityHigh, 0, 128);
+  SensorMonitoringTaskHandle = osThreadCreate(osThread(sensor_monitoring_task), NULL);
+
+  osThreadDef(alarm_management_task, alarm_management_task_init, osPriorityAboveNormal, 0, 128);
+  AlarmManagementTaskHandle = osThreadCreate(osThread(alarm_management_task), NULL);
+
+  osThreadDef(communication_task, communication_task_init, osPriorityNormal, 0, 128);
+  CommunicationTaskHandle = osThreadCreate(osThread(communication_task), NULL);
+
+  osThreadDef(ui_task, ui_task_init, osPriorityLow, 0, 128);
+  UserInterfaceTaskHandle = osThreadCreate(osThread(ui_task), NULL);
+
   /* USER CODE END 2 */
+
+  /* Start scheduler*/
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler*/
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-	while (1) {
-		sprintf((char*) uart_txd_buffer, "HOURS\tMINS\tSECS\r\n");
-		HAL_UART_Transmit(&huart6, uart_txd_buffer, strlen((char*)uart_txd_buffer), 1000);
-		HAL_Delay(100);
-	/* USER CODE END WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -141,9 +187,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -156,10 +201,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -185,14 +230,13 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
-  * @brief USART6 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART6 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART6_UART_Init(void)
 {
 
@@ -218,19 +262,18 @@ static void MX_USART6_UART_Init(void)
   /* USER CODE BEGIN USART6_Init 2 */
 
   /* USER CODE END USART6_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -254,36 +297,116 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
+void sensor_monitoring_task_init(void const *argument)
+{
+  while (1)
+  {
+    HAL_ADC_Start(&hadc1);
+
+    if (HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY) == HAL_OK)
+    {
+      adcValue = HAL_ADC_GetValue(&hadc1);
+      mappedValue = (adcValue * 1024) / 4095;
+
+      if (mappedValue > FIRE_THRESHOLD)
+      {
+        FIRE_DETECTED = 1;
+      }
+      else
+      {
+        FIRE_DETECTED = 0;
+      }
+    }
+
+    HAL_Delay(100);
+  }
+}
+void alarm_management_task_init(void const *argument)
+{
+  while (1)
+  {
+    if (FIRE_DETECTED)
+    {
+      HAL_GPIO_WritePin(GPIOA, LD2_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);
+      HAL_Delay(750);
+      HAL_GPIO_WritePin(GPIOA, LD2_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+      HAL_Delay(750);
+    }
+  }
+}
+void communication_task_init(void const *argument)
+{
+  while (1)
+  {
+    if (FIRE_DETECTED)
+    {
+      sprintf((char *)txd_msg_buffer, "Fire Alert!");
+      HAL_UART_Transmit(&huart6, txd_msg_buffer, strlen((char *)txd_msg_buffer), HAL_MAX_DELAY);
+      HAL_Delay(1000);
+    }
+  }
+}
+void ui_task_init(void const *argument)
+{
+  while (1)
+  {
+    if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1) == GPIO_PIN_RESET)
+    {
+      FIRE_DETECTED = 0;
+      HAL_Delay(500);
+    }
+
+    if (FIRE_DETECTED)
+    {
+      clear();
+      setCursor(0, 0);
+      print("FIRE! FIRE!");
+      setCursor(0, 1);
+      print("FIRE DETECTED");
+    }
+    else
+    {
+      clear();
+      setCursor(0, 0);
+      print("Status: OK");
+    }
+
+    HAL_Delay(1000);
+  }
+}
 
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-	/* User can add his own implementation to report the HAL error return state */
-	__disable_irq();
-	while (1) {
-	}
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
